edb8f1cd87aa5bbf55b84d00e3959e04dc1385d0
osamuharu <huynhnamkha1010@gmail.com>
2026-05-21 20:02:21 +0700

    chore: set up environment configuration and remove unused app files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from './shared/types/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService<AllConfigType>);
   const host = 'localhost';
-  const port = process.env.PORT ?? 3000;
+  const port = configService.getOrThrow('app.port', { infer: true });
+  const appName = configService.getOrThrow('app.name', { infer: true });
 
   await app.listen(port);
 
   return {
+    appName: appName,
     url: `http://${host}:${port}`,
   };
 }
 bootstrap()
-  .then(({ url }) => {
+  .then(({ appName, url }) => {
+    console.log(appName);
     console.log(`Server run on ${url}`);
   })
   .catch((e: Error) => {
