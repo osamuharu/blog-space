91dd602d874fd5f229472f3eaecf12b1d4faa6dd
osamuharu <huynhnamkha1010@gmail.com>
2026-05-23 08:23:20 +0700

    feat: add default now

--- backend/src/shared/entities/timestamps-document.entity.ts
+++ backend/src/shared/entities/timestamps-document.entity.ts
@@ -1,10 +1,11 @@
 import { Prop } from '@nestjs/mongoose';
+import { now } from 'mongoose';
 
 export class TimestampsDocumentEntity {
-  @Prop({ type: Date })
+  @Prop({ type: Date, default: now })
   createdAt: Date;
 
-  @Prop({ type: Date })
+  @Prop({ type: Date, default: now })
   updatedAt: Date;
 
   @Prop({ type: Date })
